ab58cc47a4b9b24c4868e7c92258c623d97523b8
Nhu <phucho0237@gmail.com>
2025-02-09 09:26:32 +0700

    feat: :sparkles: add config file

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from "express";
 
+import { PORT } from "./config/env.js";
+
 const app = express();
 
 app.get("/", (req, res) => {
    res.send(`Hello API`);
 });
 
-app.listen(3000, () => {
-   console.log("API is running on http://localhost:3000");
+app.listen(PORT, () => {
+   console.log(`API is running on http://localhost:${PORT}`);
 });
 
 export default app;
